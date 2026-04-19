97640f0e19ee77eb019c12af43c6f3d9654c6fb0
garrysidhu678 <garryakliya31@gmail.com>
2026-04-19 15:16:21 +0530

    Update premium_emoji.py

--- premium_emoji.py
+++ premium_emoji.py
@@ -30,4 +30,39 @@ class PremiumEmoji:
     def premium_required():
         return (
             f"{PremiumEmoji.LOCK} <b>Access Denied</b>\n"
-            f"{Premium
+            f"{PremiumEmoji.SMALL_LINE}\n"
+            f"Only {PremiumEmoji.PREMIUM} users can use this command.\n"
+            f"Upgrade now {PremiumEmoji.ROCKET}"
+        )
+
+    @staticmethod
+    def premium_success(days):
+        return (
+            f"{PremiumEmoji.CHECK} <b>Premium Activated</b>\n"
+            f"{PremiumEmoji.SMALL_LINE}\n"
+            f"Duration: <b>{days} Days</b>\n"
+            f"Enjoy {PremiumEmoji.FIRE}"
+        )
+
+    @staticmethod
+    def premium_expired():
+        return (
+            f"{PremiumEmoji.WARNING} <b>Premium Expired</b>\n"
+            f"{PremiumEmoji.SMALL_LINE}\n"
+            f"Renew now {PremiumEmoji.ROCKET}"
+        )
+
+    @staticmethod
+    def user_status(is_premium):
+        return PremiumEmoji.BADGE if is_premium else PremiumEmoji.FREE
+
+    @staticmethod
+    def premium_menu(username):
+        return (
+            f"{PremiumEmoji.LINE}\n"
+            f"{PremiumEmoji.CROWN} <b>Premium Panel</b>\n"
+            f"{PremiumEmoji.LINE}\n\n"
+            f"User: <b>{username}</b>\n"
+            f"Status: {PremiumEmoji.BADGE}\n\n"
+            f"{PremiumEmoji.ROCKET} Enjoy unlocked features!"
+        )
